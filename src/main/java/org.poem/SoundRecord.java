c37a55dfa196ac2bc3b5c1024ed6fe85b0144bb5
lywworking <luyanwen2010>
2018-07-18 16:45:50 +0800

    add method

--- src/main/java/org.poem/SoundRecord.java
+++ src/main/java/org.poem/SoundRecord.java
@@ -3,8 +3,19 @@ package org.poem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Collection;
 
 @SpringBootApplication
+@Controller
+@RequestMapping("")
 public class SoundRecord {
 
     /**
@@ -12,6 +23,31 @@ public class SoundRecord {
      * @param args
      */
     public static void main(String[] args) {
-        SpringApplication.run(SoundRecord.class,args);
+        SpringApplication.run(SoundRecord.class, args);
+    }
+
+
+    @RequestMapping("/")
+    public String index(){
+        return  "/index";
+    }
+
+    @RequestMapping("/record")
+    public void record(HttpServletRequest request){
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+                request.getSession().getServletContext());
+        if (multipartResolver.isMultipart(request)) {
+            System.err.println("错了");
+        }
+        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+        Collection<MultipartFile> files = multiRequest.getFileMap().values();
+        for (MultipartFile file : files) {
+            try {
+                byte[] bytes = file.getBytes();
+                System.err.println(bytes.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
