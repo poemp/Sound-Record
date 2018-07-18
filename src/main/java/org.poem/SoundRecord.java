14a9ad642b7515ec32ca1879e88884dd655f907c
lywworking <luyanwen2010>
2018-07-18 17:56:09 +0800

    完成

--- src/main/java/org.poem/SoundRecord.java
+++ src/main/java/org.poem/SoundRecord.java
@@ -1,31 +1,27 @@
 package org.poem;
 
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Collection;
 
 @SpringBootApplication
 @Controller
 @RequestMapping("")
-@EnableWebMvc
-public class SoundRecord implements WebMvcConfigurer{
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/webapp/");
-    }
+public class SoundRecord{
     /**
      *
      * @param args
@@ -39,24 +35,26 @@ public class SoundRecord implements WebMvcConfigurer{
     @RequestMapping("")
     public String index(){
 
-        return  "index";
+        return  "/index.html";
     }
 
     @RequestMapping("/record")
+    @ResponseBody
     public void record(HttpServletRequest request){
-        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
-                request.getSession().getServletContext());
-        if (multipartResolver.isMultipart(request)) {
-            System.err.println("错了");
-        }
-        MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+        StandardMultipartHttpServletRequest multiRequest = (StandardMultipartHttpServletRequest) request;
         Collection<MultipartFile> files = multiRequest.getFileMap().values();
+        FileOutputStream fileOutputStream = null;
         for (MultipartFile file : files) {
             try {
                 byte[] bytes = file.getBytes();
                 System.err.println(bytes.length);
+
+                fileOutputStream = new FileOutputStream(new File(file.getOriginalFilename()));
+                fileOutputStream.write(bytes,0 , bytes.length);
             } catch (IOException e) {
                 e.printStackTrace();
+            }finally {
+                IOUtils.closeQuietly(fileOutputStream);
             }
         }
     }
